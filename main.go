999d1a3b24d5bab8efdaf57e65d247b5a1171bbd
Alex <alg@campaignmonitor.com>
2018-06-28 13:22:04 +1000

    more flags

--- main.go
+++ main.go
@@ -8,53 +8,73 @@ import (
 	"time"
 
 	"github.com/dgraph-io/badger"
+	"github.com/dgraph-io/badger/options"
+	"os"
 )
 
 type entry struct {
 	key, value []byte
 }
 
+const (
+	dataDir = "data"
+	logDir  = "logs"
+)
+
 func main() {
-	parallel := flag.Int("parallel", 10, "Number of go routines for writing data concurrently")
+	parallel := flag.Int("parallel", 1, "Number of go routines for writing data concurrently")
 	keySize := flag.Int("key-size", 64, "Key size in bytes")
 	valueSize := flag.Int("value-size", 1024, "Value size in bytes")
 	count := flag.Int("count", 10000, "Number of documents to write")
 	batchSize := flag.Int("batch-size", 1000, "The size of the batch to write data into the disk")
+	sameValue := flag.Bool("same-value", false, "If true, the value of all the keys will be the same")
+	syncWrite := flag.Bool("sync-write", false, "Sync all writes to disk")
+	clean := flag.Bool("clean", false, "Removes data files and the logs before write")
 
 	flag.Parse()
 
+	err := initialise(*clean)
+	if err != nil {
+		log.Fatalf("Failed to initialise the directories. %s", err)
+	}
+
 	opts := badger.DefaultOptions
-	opts.Dir = "data"
-	opts.ValueDir = "data/logs"
+	opts.SyncWrites = *syncWrite
+	opts.ValueLogLoadingMode = options.MemoryMap
+	opts.TableLoadingMode = options.MemoryMap
+	opts.Dir = dataDir
+	opts.ValueDir = logDir
 	db, err := badger.Open(opts)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer func() {
-		err := db.Close()
 		fmt.Println("Closing the database...")
+		err := db.Close()
 		if err != nil {
-			fmt.Printf("Err05: %s\n", err)
+			fmt.Printf("Err06: %s\n", err)
 		}
 	}()
 
-	producer := newProducer(*count, *keySize, *valueSize, *parallel)
+	producer := newProducer(*count, *keySize, *valueSize, *parallel, *sameValue)
+	fmt.Printf("Preparing %v documents...\n", *count)
+	producer.generateAll()
+	fmt.Printf("Writing %d unique keys into the database\n", producer.len())
 	wg := sync.WaitGroup{}
-	start := time.Now()
+
 	for i := 0; i < *parallel; i++ {
 		wg.Add(1)
 		go consume(&wg, producer, *batchSize, db)
 	}
-
+	start := time.Now()
 	producer.start()
 	wg.Wait()
-	now := time.Now()
+	duration := time.Now().Sub(start)
 	fmt.Printf("  Key Size: %d Bytes\n", *keySize)
 	fmt.Printf("Value Size: %d Bytes\n", *valueSize)
 	fmt.Printf("  Parallel: %d\n", *parallel)
 	fmt.Printf("     Count: %d\n", *count)
 	fmt.Printf("Batch Size: %d\n", *batchSize)
-	duration := now.Sub(start)
 	fmt.Printf("  Duration: %v\n", duration)
 	fmt.Printf(" Writes /s: %v\n", int(float64(*count)/duration.Seconds()))
 }
@@ -64,42 +84,68 @@ func consume(wg *sync.WaitGroup, p *producer, batchSize int, db *badger.DB) {
 	counter := 0
 	txn := db.NewTransaction(true)
 
-	commit := func(txn *badger.Txn, counter *int) error {
-		err := txn.Commit(nil)
-		if err != nil {
-			return err
-		}
-		*counter = 0
-		return nil
-	}
-
 	for e := range p.output() {
 		err := txn.Set(e.key, e.value)
 		if err != nil {
 			if err == badger.ErrTxnTooBig {
-				txn.Commit(nil)
+				err := txn.Commit(nil)
+				if err != nil {
+					fmt.Printf("Err01: %s\n", err)
+				}
 				txn = db.NewTransaction(true)
 				err = txn.Set(e.key, e.value)
 				if err != nil {
-					fmt.Printf("Err01: %s\n", err)
+					fmt.Printf("Err02: %s\n", err)
 					return
 				}
+				counter = 1
 				continue
 			}
 			//err != badger.ErrTxnTooBig
-			fmt.Printf("Err02: %s\n", err)
+			fmt.Printf("Err03: %s\n", err)
 			return
 		}
+		counter++
 		if counter >= batchSize {
-			if err = commit(txn, &counter); err != nil {
-				fmt.Printf("Err03: %s\n", err)
+			if err = txn.Commit(nil); err != nil {
+				fmt.Printf("Err04: %s\n", err)
 				return
 			}
+			counter = 0
+			txn = db.NewTransaction(true)
 		}
 	}
 
-	if err := commit(txn, &counter); err != nil {
-		fmt.Printf("Err04: %s\n", err)
-		return
+	err := txn.Commit(nil)
+	if err != nil {
+		fmt.Printf("Err05: %s\n", err)
 	}
+}
+
+func initialise(clean bool) error {
+	if err := createIfNotExist(dataDir, clean); err != nil {
+		return err
+	}
+
+	return createIfNotExist(logDir, clean)
+}
+
+func createIfNotExist(dir string, clean bool) error {
+	_, err := os.Stat(dir)
+	if os.IsNotExist(err) {
+		return os.MkdirAll(dir, 0777)
+	}
+	if err != nil {
+		return err
+	}
+
+	if !clean {
+		return nil
+	}
+	fmt.Printf("Cleaning up %s directory...\n", dir)
+	err = os.RemoveAll(dir)
+	if err != nil {
+		return err
+	}
+	return os.MkdirAll(dir, 0777)
 }
--- producer.go
+++ producer.go
@@ -4,32 +4,61 @@ import "math/rand"
 
 type producer struct {
 	count, keySize, valueSize int
-	pipe chan *entry
+	pipe                      chan *entry
+	input                     []*entry
+	value                     []byte
+	sameValue                 bool
 }
 
-func newProducer(count, keySize, valueSize, parallel int) *producer {
-	return &producer{
+func newProducer(count, keySize, valueSize, parallel int, sameValue bool) *producer {
+	p := &producer{
 		count:count,
 		keySize:keySize,
 		valueSize:valueSize,
 		pipe: make(chan *entry, parallel),
+		input:make([]*entry, count),
 	}
+
+	if sameValue {
+		p.value = make([]byte, valueSize)
+		rand.Read(p.value)
+	}
+	return p
 }
 
 func (p *producer) output() <-chan *entry  {
 	return p.pipe
 }
 
+func (p *producer) len() int {
+	return len(p.input)
+}
 
 func (p *producer) start() {
 	defer close(p.pipe)
-	for i := 0; i < p.count; i++ {
-		p.pipe <- generate(p.keySize, p.valueSize)
+	for _, e := range p.input {
+		p.pipe <- e
 	}
 }
 
-func generate(keySize, valueSize int) *entry {
+func (p *producer) generateAll() {
+	m  := make(map[string]struct{})
+	for i := 0; i < p.count; {
+		e := p.generate(p.keySize, p.valueSize)
+		if _, ok := m[string(e.key)]; !ok {
+			p.input[i] = e
+			m[string(e.key)] = struct{}{}
+			i++
+		}
+		continue
+	}
+}
+
+func (p *producer) generate(keySize, valueSize int) *entry {
 	key := make([]byte, keySize)
+	if p.sameValue {
+		return &entry{key: key, value: p.value}
+	}
 	value := make([]byte, valueSize)
 	rand.Read(key)
 	rand.Read(value)
